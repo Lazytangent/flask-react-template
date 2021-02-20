274349f8ab1d856361ee7cdeee0488fc9e9518af
Peter Mai <peter.tn.mai@gmail.com>
2021-02-20 10:33:12 -0600

    Update auth components to use Redux store

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch } from 'react-redux';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
-import { authenticate } from "./services/auth";
+import { authenticate } from './store/session';
 
 function App() {
+  const dispatch = useDispatch();
+
   const [authenticated, setAuthenticated] = useState(false);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    (async() => {
-      const user = await authenticate();
+    (async () => {
+      const user = await dispatch(authenticate());
       if (!user.errors) {
         setAuthenticated(true);
       }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 
+import configureStore from './store';
+import * as sessionActions from './store/session';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
+
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
