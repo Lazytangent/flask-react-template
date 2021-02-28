8f0cb8798e7c81e0a234c3afd71d2eb56f262b4b
Lazytangent <pm123633@gmail.com>
2021-02-27 18:40:42 -0600

    Update index.js

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
+import configureStore from './store';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
 
 const Root = () => (
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 ReactDOM.render(
