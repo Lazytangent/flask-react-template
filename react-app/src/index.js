6bf7503e3b805f7f83e3a15c3d691ce48576f991
Lazytangent <pm123633@gmail.com>
2021-02-27 18:48:18 -0600

    Update content of App and move BrowserRouter

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -13,9 +14,11 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const Root = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
 
 ReactDOM.render(
